46e3de469514f9b3c26614ead5fad7099a9e54d4
Faustino Kialungila <faustino.kialungila@gmail.com>
2020-05-01 21:56:36 +0200

    First commit yay

---
+++ index.ts
@@ -0,0 +1,52 @@
+// @ts-nocheck
+import {traverse, parse} from '@babel/core';
+import * as t from '@babel/types';
+import generator from '@babel/generator';
+
+const code = `
+function add(a, b) {
+  $log;
+  const result = a + b;
+
+  return result;
+}
+add(2, 3);
+`;
+
+const ast = parse(code);
+
+let hasLogID = false
+
+const LoggerVisitor = {
+  ReturnStatement: (path) => {
+    const identifierName = path.node.argument.name;
+    if(path.node.argument.type === 'Identifier' && hasLogID) {
+     
+      path.replaceWithMultiple([
+        t.identifier(`console.log('Final Result 😛 ==> ', ${identifierName})`),
+        t.identifier(`return ${identifierName}`)
+      ]);
+    }
+  },
+  Identifier: (path) => {
+    if(path.isIdentifier({name: '$log'})) {
+      hasLogID = true;
+      path.remove();
+    }
+  },
+  BinaryExpression: (path) => {
+    if(path.node.operator === '+') {
+      path.node.operator = '*'
+    }
+  }
+}
+
+traverse(ast, { 
+  enter: (path) => {
+    path.traverse(LoggerVisitor);
+  }
+});
+
+const output = generator(ast, {}, code);
+const newCode = output.code;
+console.log({newCode: eval(newCode)})
---
+++ readme.md
@@ -0,0 +1,41 @@
+### Babel Plugin 🔀 
+
+Playing with Babel Plugins and trying to hackly replace the key `$log` with an actual `console.log` statement.
+
+Pair programming with: [Bruno Kiafuka](https://github.com/brunokiafuka)
+
+It converts this:
+
+```js
+function add(a, b) {
+  $log;
+  const result = a + b;
+
+  return result;
+}
+add(2, 3);
+```
+
+Into this:
+
+```js
+function add(a, b) {
+  const result = a + b;
+  
+  console.log('Final Result 😛 ==> ', result)
+  
+  return result;
+}
+add(2, 3);
+```
+
+
+
+### Todo
+-[ ] Improve Docs
+
+### Roadmap 🛣
+- [ ]  Publish to NPM
+
+
+
